ba0e758ba7cac7cbf235b89d12fdec9795862ef4
Leonardo Costa <lbcosta.dev@gmail.com>
2022-03-14 21:52:22 -0300

    Building Blockchain in Go. Part 1: Basic Prototype

---
+++ go.mod
@@ -0,0 +1,3 @@
+module parabuains
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+type Blockchain struct {
+	blocks []*Block // TODO: Use map of arrays to make a hash->block search
+}
+
+// AddBlock is a Proof-Of-Work free method
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func main() {
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("$$$$$$$$$$ Parabuains (PRBS)! $$$$$$$$$$")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println()
+
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 PRBS to Allan")
+	bc.AddBlock("Send 2 more PRBS to Allan")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
